9b7bf82d6c576efcd9ab7b7c90f9d69336e60fbb
Ian Yates <ian.yates@neofintechlab.com>
2021-06-01 17:54:32 +0100

    Tested DiffSynonymCreate

---
+++ DacpacDiff.CoreTests/Diff/DiffSynonymCreateTests.cs
@@ -0,0 +1,35 @@
+﻿using DacpacDiff.Core.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DacpacDiff.Core.Diff.Tests
+{
+    [TestClass]
+    public class DiffSynonymCreateTests
+    {
+        [TestMethod]
+        public void DiffSynonymCreate()
+        {
+            // Arrange
+            var syn = new SynonymModel(new SchemaModel(DatabaseModel.Empty, "LSchema"), "LSynonym", "[TSchema].[TObject]");
+
+            // Act
+            var diff = new DiffSynonymCreate(syn);
+
+            // Assert
+            Assert.AreSame(syn, diff.Synonym);
+            Assert.AreSame(syn, diff.Model);
+            Assert.AreEqual("Create synonym", diff.Title);
+            Assert.AreEqual("[LSchema].[LSynonym]", diff.Name);
+        }
+
+        [TestMethod]
+        public void DiffSynonymCreate__Null_arg__Fail()
+        {
+            // Act
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+            Assert.ThrowsException<ArgumentNullException>(() => new DiffSynonymCreate(null));
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+        }
+    }
+}
